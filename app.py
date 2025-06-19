b24718c16dfa62deec7c043aff1761bbbcd2498b
Zakhar Bybko <zakhar.bybko@icloud.com>
2025-06-20 00:46:06 +0300

    feat: Initialize database schema and user management functions
    - Created a new SQL schema for users and subscriptions in database.sql.
    - Implemented user management functions in functions.py, including adding and deleting users from a Telegram channel.
    - Loaded environment variables in loader.py for configuration.
    - Added requirements for necessary packages in requirements.txt.
    - Configured Python virtual environment settings in pyvenv.cfg.

---
+++ app.py
@@ -0,0 +1,228 @@
+import json
+import time
+from datetime import datetime, timedelta
+
+
+from flask import Flask, render_template_string, request, jsonify
+
+from functions import generate_merchant_signature, extract_user_id_from_reference, add_user_to_channel, \
+    generate_signature, delete_user_from_channel
+from loader import MERCHANT_ACCOUNT, MERCHANT_DOMAIN, db
+
+
+app = Flask(__name__)
+
+@app.route('/add_user')
+def add_user():
+    add_user_to_channel(7559268811)
+    return "200"
+@app.route('/delete')
+def delete():
+    delete_user_from_channel(7559268811)
+    return "200"
+@app.route('/')
+def index():
+    html_template = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Kosovska</title>
+<style>
+* {
+margin: 0;
+padding: 0;
+box-sizing: border-box;
+}
+
+body {
+display: flex;
+justify-content: center;
+align-items: center;
+height: 100vh;
+background-color: #ffffff;
+font-family: Arial, sans-serif;
+}
+
+.container {
+display: flex;
+width: 90%;
+max-width: 600px;
+flex-direction: column;
+align-items: center;
+gap: 20px;
+background-color: #eeeeee;
+padding: 40px;
+border-radius: 10px;
+box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+}
+
+.btn {
+width: 100%;
+display: inline-block;
+padding: 15px 15px;
+border-radius: 50px;
+border: 1px solid #050505;
+text-decoration: none;
+text-align: center;
+font-size: 18px;
+font-weight: bold;
+color: #050505;
+cursor: pointer;
+transition: background-color 0.3s ease;
+}
+.lookbook__btn{
+    background-color: #000000;
+    border: 1px solid #ffffff;
+}
+.lookbook__btn_white {
+    color: #ffffff;
+}
+.lookbook__btn_red {
+    color: #b84b27;
+}
+</style>
+</head>
+<body>
+
+<div class="container">
+<a href="https://t.me/sveta_kosovska" class="btn btn-telegram">Telegram</a>
+<a href="https://www.instagram.com/sveta_kosovska" class="btn btn-instagram">Instagram</a>       
+<a href="https://t.me/+i8D-g8m5_Ak2MDNi" class="btn lookbook__btn lookbook__btn_white">LookBook</a>
+<a href="https://ssk24test.my.canva.site/daglhozlvg0" class="btn btn-other">Ціни на послуги</a>
+<a href="https://t.me/kosovskamanager" class="btn btn-support">Підтримка</a>
+</div>
+
+</body>
+</html>
+
+    """
+    return render_template_string(html_template)
+
+
+@app.route('/payment_callback', methods=['POST'])
+def callback():
+    data_str = request.form.to_dict()
+
+    # Проверка входных данных
+    if not data_str:
+        return jsonify({"error": "No data received"}), 400
+
+    try:
+        data_json_str = list(data_str.keys())[0]
+        data = json.loads(data_json_str)
+    except (IndexError, json.JSONDecodeError):
+        return jsonify({"error": "Invalid JSON data"}), 400
+
+    # Основные данные
+    order_reference = data.get('orderReference')
+    if not order_reference:
+        return jsonify({"error": "Missing order reference"}), 400
+
+    processing_time = int(time.time())
+    status = 'accept'
+    user_id = extract_user_id_from_reference(order_reference)
+    signature = generate_signature(order_reference, status, processing_time)
+
+    def generate_response():
+        return {
+            "orderReference": order_reference,
+            "status": status,
+            "time": processing_time,
+            "signature": signature
+        }
+
+    # Обработка статусов
+    transaction_status = data.get("transactionStatus")
+    if transaction_status == "Approved":
+        payment_sys = data.get('paymentSystem')
+        if not db.get_subs(user_id):
+            add_user_to_channel(user_id)
+            db.add_subs(user_id, payment_sys)
+        else:
+            add_user_to_channel(user_id)
+            db.update_subs(user_id)
+        print(data)
+        return jsonify(generate_response()), 200
+
+    elif transaction_status == "Declined":
+        delete_user_from_channel(user_id)
+        print(data)
+        return jsonify(generate_response()), 200
+
+    elif transaction_status == "Expired":
+        delete_user_from_channel(user_id)
+        print(data)
+        return jsonify(generate_response()), 200
+
+    elif transaction_status == "Refunded":
+        delete_user_from_channel(user_id)
+        print(data)
+        return jsonify(generate_response()), 200
+
+    # Если статус не распознан
+    return jsonify({"response": "Payment failed!"}), 400
+
+
+@app.route('/pay/<int:user_id>')
+def pay(user_id):
+    amount = 599
+    currency = "UAH"
+    product_name = ["Subscription to Telegram Channel"]
+    product_price = [599]
+    product_count = [1]
+    order_date = int(time.time())
+    today = datetime.now()
+    future_date = today + timedelta(days=30)
+    date_next = future_date.strftime("%d.%m.20%y")
+    order_reference = f"invoice_{user_id}_{order_date}"
+
+    # Генеруємо підпис
+    signature = generate_merchant_signature(
+        MERCHANT_ACCOUNT,
+        MERCHANT_DOMAIN,
+        order_reference,
+        order_date,
+        amount,
+        currency,
+        product_name,
+        product_price,
+        product_count
+    )
+
+    html_content = f"""
+    <html>
+      <body>
+        <form id="wayforpay_form" action="https://secure.wayforpay.com/pay" method="POST">
+            <input type="hidden" name="merchantAccount" value="{MERCHANT_ACCOUNT}">
+            <input type="hidden" name="merchantDomainName" value="{MERCHANT_DOMAIN}">
+            <input type="hidden" name="serviceUrl" value="{MERCHANT_DOMAIN}/payment_callback">
+            <input type="hidden" name="orderReference" value="{order_reference}">
+            <input type="hidden" name="orderDate" value="{order_date}">
+            <input type="hidden" name="amount" value="{amount}">
+            <input type="hidden" name="currency" value="UAH">
+            <input type="hidden" name="productName[]" value="{product_name[0]}">
+            <input type="hidden" name="productPrice[]" value="{amount}">
+            <input type="hidden" name="productCount[]" value="1">
+            <input type="hidden" name="regularOn" value="1">
+            <input type="hidden" name="regularBehavior" value="preset">
+            <input type="hidden" name="regularAmount" value="{amount}">
+            <input type="hidden" name="regularMode" value="monthly">
+            <input type="hidden" name="regularCount" value="24">
+            <input type="hidden" name="dateNext" value="{date_next}">
+            <input type="hidden" name="merchantSignature" value="{signature}">
+            <input type="submit" value="Оплатити">
+        </form>
+
+        <script type="text/javascript">
+            document.getElementById('wayforpay_form').submit();
+        </script>
+      </body>
+    </html>
+    """
+    return render_template_string(html_content)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
---
+++ functions.py
@@ -0,0 +1,113 @@
+import time
+
+from loader import SECRET_KEY, BOT_TOKEN, CHANNEL_ID, ADMIN_ID, db
+import hmac
+import hashlib
+import requests
+
+
+def generate_merchant_signature(merchant_account, merchant_domain, order_reference, order_date, amount, currency, product_name, product_price, product_count):
+    signature_string = f"{merchant_account};{merchant_domain};{order_reference};{order_date};{amount};{currency};"
+    signature_string += f"{';'.join(product_name)};{';'.join(map(str, product_count))};{';'.join(map(str, product_price))}"
+    hash_signature = hmac.new(SECRET_KEY.encode('utf-8'), signature_string.encode('utf-8'), digestmod='md5').hexdigest()
+    return hash_signature
+
+
+def generate_signature(order_reference, status, current_time, secret_key=SECRET_KEY):
+    data_string = f"{order_reference};{status};{current_time}"
+    signature = hmac.new(secret_key.encode('utf-8'), data_string.encode('utf-8'), digestmod='md5').hexdigest()
+    return signature
+
+
+def extract_user_id_from_reference(order_reference):
+    return order_reference.split("_")[1]
+
+
+def generate_short_link_name(user_id):
+    unique_string = f"user_{user_id}_{int(time.time())}"
+    short_name = hashlib.md5(unique_string.encode()).hexdigest()[:32]
+    return short_name
+
+def add_user_to_channel(user_id):
+    dbuser = db.get_user(user_id)
+
+    # URL для створення посилання
+    invite_link_url = f"https://api.telegram.org/bot{BOT_TOKEN}/createChatInviteLink"
+
+    # Параметри посилання
+    invite_link_params = {
+        "chat_id": CHANNEL_ID,
+        "name": generate_short_link_name(user_id),
+        "member_limit": 1,  # Обмеження: лише один користувач
+        "creates_join_request": False
+    }
+
+    # Відправлення запиту на створення посилання
+    response = requests.post(invite_link_url, json=invite_link_params)
+
+    if response.status_code == 200:
+        invite_link = response.json().get('result', {}).get('invite_link')
+
+        if invite_link:
+            # Відправлення посилання користувачу
+            message = (
+                "Дякуємо за оплату! Ваша місячна підписка на канал LookBook активована.\n\n"
+                f"[Перейти до каналу]({invite_link})"
+            )
+
+            user_response = requests.get(
+                f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+                params={
+                    "chat_id": user_id,
+                    "text": message,
+                    "parse_mode": "Markdown"
+                }
+            )
+
+            if user_response.status_code == 200:
+                # Повідомлення адміністратору
+                admin_message = f"Користувач @{dbuser[0]} - {dbuser[1]} успішно доданий до каналу."
+                requests.get(
+                    f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+                    params={
+                        "chat_id": ADMIN_ID,
+                        "text": admin_message
+                    }
+                )
+            else:
+                # Помилка надсилання повідомлення користувачу
+                print(f"Помилка надсилання повідомлення: {user_response.json()}")
+    else:
+        # Помилка створення посилання
+        error_message = f"Помилка створення запрошення: {response.json()}"
+        requests.get(
+            f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+            params={
+                "chat_id": ADMIN_ID,
+                "text": error_message
+            }
+        )
+
+def delete_user_from_channel(user_id):
+    dbuser = db.get_user(user_id)
+    ban_url = f'https://api.telegram.org/bot{BOT_TOKEN}/banChatMember'
+    ban_params = {
+        'chat_id': CHANNEL_ID,
+        'user_id': user_id
+    }
+    ban_response = requests.post(ban_url, params=ban_params)
+    db.delete_user(user_id)
+
+    if ban_response.status_code == 200:
+        unban_url = f'https://api.telegram.org/bot{BOT_TOKEN}/unbanChatMember'
+        unban_params = {
+            'chat_id': CHANNEL_ID,
+            'user_id': user_id
+        }
+        unban_response = requests.post(unban_url, params=unban_params)
+        print("Користувача видалено з каналу.")
+        requests.get(
+            f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={ADMIN_ID}&text=Користувачa @{dbuser[0]} - {dbuser[1]} видалено каналу!")
+    else:
+        print("Помилка при видаленні користувача:", ban_response.json())
+
